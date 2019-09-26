bd8f305815e0d174646ec6f33506b35ebacd038c
Adam Asher <asheradamr@gmail.com>
2019-09-26 10:29:56 -0500

    Finished assignment

--- recursionLearn/Program.cs
+++ recursionLearn/Program.cs
@@ -10,18 +10,19 @@ namespace recursionLearn
 
             Console.WriteLine(Factorial(5));
             Console.WriteLine(Factorial(10));
-
+            Console.WriteLine(S(11));
+            
         }
 
-        static int Factorial(int n)
-        {
-            if (n == 0)
-            {
-                return 1;
-            }
-
-            // recursive solution (as opposed to iterative)
-            return n * Factorial(n - 1);
+        static int Factorial(int n)         //New method for Factorials                                                              <       
+        {                                    //                                                                                      | 
+            if (n == 0)                     //IF the int n is equal to 0 return 1                                                    |   
+            {                               //                                                                                       |
+                return 1;                   //return one                                                                             |
+            }                              //                                                                                        |
+            //                                                                                                                       |
+            // recursive solution (as opposed to iterative)                                                                          |
+            return n * Factorial(n - 1);    //[Factorial] is the recursive part in the solution because its refering back to it self /
 
             // e.g.
             // 5! = 5 * 4!
@@ -30,6 +31,13 @@ namespace recursionLearn
             //    2! = 2 * 1!
             //     1! = 1 * 0!
             //      0! = 1 (because math)
+        }
+        static int S(int n)
+        {
+            if (n == 1)
+                return 5;
+            else
+                return 5 * n + S(n - 1);
         }
     }
 }
